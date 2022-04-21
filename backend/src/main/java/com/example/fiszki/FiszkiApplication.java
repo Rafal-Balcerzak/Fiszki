2f9096795379b64af2c9052687e4987f4a620d6c
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2022-04-21 23:34:37 +0200

    - initial commit

---
+++ backend/src/main/java/com/example/fiszki/FiszkiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fiszki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiszkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiszkiApplication.class, args);
+    }
+
+}
